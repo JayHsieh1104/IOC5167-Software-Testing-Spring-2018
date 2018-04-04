295e2fc07ecb3c6a21b87a2dd247130b934ac5a7
Jay Hsieh <jj011644@gmail.com>
2018-04-04 08:51:24 -0700

    add header file Triangle_Problem.h

---
+++ HW1/Triangle_Problem/Triangle_Problem.h
@@ -0,0 +1,10 @@
+#ifndef GTEST_TRIANGLE_PROBLEM_H_
+#define GTEST TRIANGLE_PROBLEM_H_
+
+// Scan the three input numbers.
+void Input(int a,int b,int c);
+
+// Print the outcome message.
+string Ouput();
+
+#endif //GTEST_TRIANGLE_PROBLEM_H_
