cc2ec5cf90339c7b3137a1e869645526faf48caa
Jay <jj011644@gmail.com>
2018-04-09 19:38:27 +0800

    add triangle problem feature code

--- HW1/Triangle_Problem/Triangle_Problem.cc
+++ HW1/Triangle_Problem/Triangle_Problem.cc
@@ -3,7 +3,16 @@
 
 using namespace std;
 
-char* Solve_Triangle_Problem(int a, int b, int c) {
-    return "feature code has not developed";
+char*  Solve_Triangle_Problem(int a, int b, int c) {
+	if (a < 1 || b < 1 || c < 1 || a > 200 || b > 200 || c > 200)
+		return "Invalid Input";
+	if( !(a + b > c && a + c > b && b + c > a) )
+		return "Not a triangle";
+	if( a==b || b==c || c==a){
+		if (a==b && b==c && c==a)
+			return "Equilateral";
+		else
+			return "Isosceles";
+	}
+	return "Scalene";
 }
-
