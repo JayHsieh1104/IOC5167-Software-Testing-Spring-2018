65975ea136b7bdda7afcbedaff7f44e76cf760d1
Jay <jj011644@gmail.com>
2018-04-09 20:17:44 +0800

    Finish initial feature code for nextdate problem

---
+++ HW1/NextDate_Problem/NextDate_Problem.cc
@@ -0,0 +1,8 @@
+#include "NextDate_Problem.h"
+#include <cstdio>
+
+using namespace std;
+
+char* Solve_NextDate_Problem(int a, int b, int c) {
+	return "feature code has not developed";
+}
---
+++ HW1/NextDate_Problem/NextDate_Problem.h
@@ -0,0 +1,9 @@
+#ifndef GTEST_NEXTDATE_PROBLEM_H_
+#define GTEST_NEXTDATE_PROBLEM_H_
+
+#include <string>
+
+// Print the outcome message based on the three input numbers.
+char* Solve_NextDate_Problem(int a, int b, int c);
+
+#endif //GTEST_NEXTDATE_PROBLEM_H_
