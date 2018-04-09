d377593d73cd036f6609692ef0310fa95e102601
Jay <jj011644@gmail.com>
2018-04-09 19:58:39 +0800

    fix typo

--- HW1/Triangle_Problem/Triangle_Problem.cc
+++ HW1/Triangle_Problem/Triangle_Problem.cc
@@ -3,7 +3,7 @@
 
 using namespace std;
 
-char*  Solve_Triangle_Problem(int a, int b, int c) {
+char* Solve_Triangle_Problem(int a, int b, int c) {
 	if (a < 1 || b < 1 || c < 1 || a > 200 || b > 200 || c > 200)
 		return "Invalid Input";
 	if( !(a + b > c && a + c > b && b + c > a) )
