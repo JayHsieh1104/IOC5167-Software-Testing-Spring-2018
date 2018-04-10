807f24ea99533341274ada47fbe39653714745c1
Jay <jj011644@gmail.com>
2018-04-10 22:47:18 +0800

    add condition statements for nextdate problem

--- HW1/NextDate_Problem/NextDate_Problem.cc
+++ HW1/NextDate_Problem/NextDate_Problem.cc
@@ -3,6 +3,10 @@
 
 using namespace std;
 
-char* Solve_NextDate_Problem(int a, int b, int c) {
+// c1. 1 <= day <= 31
+// c2. 1 <= month <= 12
+// c3. 1812 <= year <= 2012
+
+char* Solve_NextDate_Problem(int year, int month, int day) {
 	return "feature code has not developed";
 }
