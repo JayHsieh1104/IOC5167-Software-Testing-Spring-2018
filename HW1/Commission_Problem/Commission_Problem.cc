7018e1145c16f37893a92ab9968f25bd93dd682b
Jay <jj011644@gmail.com>
2018-04-12 10:33:47 +0800

    initialize .cc file

---
+++ HW1/Commission_Problem/Commission_Problem.cc
@@ -0,0 +1,10 @@
+#include "Commission_Problem.h"
+#include <cstdio>
+#include <cstring>
+
+using namespace std;
+
+double Solve_Commission_Problem(int lock, int stock, int barrel) {
+	return 0;
+}
+
