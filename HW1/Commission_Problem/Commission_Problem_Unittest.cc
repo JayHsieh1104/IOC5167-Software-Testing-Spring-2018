2100c459f82ac7f0254f77048af2b621336fbb4d
Jay <jj011644@gmail.com>
2018-04-12 20:54:33 +0800

    add EdgeTest, DecisionTableTest

--- HW1/Commission_Problem/Commission_Problem_Unittest.cc
+++ HW1/Commission_Problem/Commission_Problem_Unittest.cc
@@ -8,7 +8,7 @@ namespace {
 // 1 ≤ bodies ≤ 90
 // output -1 = invalid input
 
-TEST(Solve_NextDate_Problem, NormalBoundaryTest) {
+TEST(Solve_Commission_Problem, NormalBoundaryTest) {
 	// Test in normal boundary value form.
 	EXPECT_EQ(2800, Solve_Commission_Problem(35, 40, 1)); // Barrel min
 	EXPECT_EQ(2825, Solve_Commission_Problem(35, 40, 2)); // Barrel min+
@@ -25,7 +25,7 @@ TEST(Solve_NextDate_Problem, NormalBoundaryTest) {
 	EXPECT_EQ(3900, Solve_Commission_Problem(35, 40, 45)); // all normal
 }
 
-TEST(Solve_NextDate_Problem, RobustBoundaryTest) {
+TEST(Solve_Commission_Problem, RobustBoundaryTest) {
 	// Test in robust boundary value form.
 	EXPECT_EQ(-1, Solve_Commission_Problem(35, 40, 0)); // Barrel min-
 	EXPECT_EQ(-1, Solve_Commission_Problem(35, 40, 91)); // Barrel max+
@@ -39,35 +39,77 @@ TEST(Solve_NextDate_Problem, RobustBoundaryTest) {
 	EXPECT_EQ(-1, Solve_Commission_Problem(0, 0, 0)); // all min-
 }
 
-TEST(Solve_NextDate_Problem, WeakNormalEquivalenceTest) {
+TEST(Solve_Commission_Problem, WeakNormalEquivalenceTest) {
   // Test in weak normal equivalence class form.
-
+	EXPECT_EQ(3900, Solve_Commission_Problem(35, 40, 45)); 
 }
 
-TEST(Solve_NextDate_Problem, StrongNormalEquivalenceTest) {
+TEST(Solve_Commission_Problem, StrongNormalEquivalenceTest) {
   // Test in strong normal equivalence class form.
-
+	EXPECT_EQ(3900, Solve_Commission_Problem(35, 40, 45)); 
 }
 
-
-TEST(Solve_NextDate_Problem, WeakRobustEquivalenceTest) {
+TEST(Solve_Commission_Problem, WeakRobustEquivalenceTest) {
   // Test in weak robust equivalence class form.
-
+	EXPECT_EQ(-1, Solve_Commission_Problem(0, 40, 45)); 
+	EXPECT_EQ(-1, Solve_Commission_Problem(71, 40, 45)); 
+	EXPECT_EQ(-1, Solve_Commission_Problem(35, 0, 45)); 
+	EXPECT_EQ(-1, Solve_Commission_Problem(35, 81, 45)); 
+	EXPECT_EQ(-1, Solve_Commission_Problem(35, 40, 0)); 
+	EXPECT_EQ(-1, Solve_Commission_Problem(35, 40, 91)); 
 }
 
-TEST(Solve_NextDate_Problem, StrongRobustEquivalenceTest) {
+TEST(Solve_Commission_Problem, StrongRobustEquivalenceTest) {
   // Test in strong robust equivalence class form.
-
+	EXPECT_EQ(-1, Solve_Commission_Problem(0, 40, 45)); 
+	EXPECT_EQ(-1, Solve_Commission_Problem(35, 0, 45)); 
+	EXPECT_EQ(-1, Solve_Commission_Problem(35, 40, 0)); 
+	EXPECT_EQ(-1, Solve_Commission_Problem(0, 0, 45)); 
+	EXPECT_EQ(-1, Solve_Commission_Problem(0, 40, 0)); 
+	EXPECT_EQ(-1, Solve_Commission_Problem(35, 0, 0)); 
+	EXPECT_EQ(-1, Solve_Commission_Problem(0, 0, 0)); 
 }
 
-TEST(Solve_NextDate_Problem, EdgeTest) {
+TEST(Solve_Commission_Problem, EdgeTest) {
   // Test in edge testing form.
-
+    EXPECT_EQ(-1, Solve_Commission_Problem(-3, 10, 10));
+    EXPECT_EQ(-1, Solve_Commission_Problem(-2, 10, 10));
+    EXPECT_EQ(0, Solve_Commission_Problem(-1, 10, 10));
+    EXPECT_EQ(-1, Solve_Commission_Problem(0, 10, 10));
+    EXPECT_EQ(59.5, Solve_Commission_Problem(1, 10, 10));
+    EXPECT_EQ(64, Solve_Commission_Problem(2, 10, 10));
+    EXPECT_EQ(591, Solve_Commission_Problem(69, 10, 10));
+    EXPECT_EQ(600, Solve_Commission_Problem(70, 10, 10));
+    EXPECT_EQ(-1, Solve_Commission_Problem(71, 10, 10));
+    EXPECT_EQ(-1, Solve_Commission_Problem(72, 10, 10));
+    EXPECT_EQ(-1, Solve_Commission_Problem(10, -1, 10));
+    EXPECT_EQ(-1, Solve_Commission_Problem(10, 0, 10));
+    EXPECT_EQ(73, Solve_Commission_Problem(10, 1, 10));
+    EXPECT_EQ(76, Solve_Commission_Problem(10, 2, 10));
+    EXPECT_EQ(474, Solve_Commission_Problem(10, 79, 10));
+    EXPECT_EQ(480, Solve_Commission_Problem(10, 80, 10));
+    EXPECT_EQ(-1, Solve_Commission_Problem(10, 81, 10));
+    EXPECT_EQ(-1, Solve_Commission_Problem(10, 82, 10));
+    EXPECT_EQ(-1, Solve_Commission_Problem(10, 10, -1));
+    EXPECT_EQ(-1, Solve_Commission_Problem(10, 10, 0));
+    EXPECT_EQ(77.5, Solve_Commission_Problem(10, 10, 1));
+    EXPECT_EQ(80, Solve_Commission_Problem(10, 10, 2));
+    EXPECT_EQ(455, Solve_Commission_Problem(10, 10, 89));
+    EXPECT_EQ(460, Solve_Commission_Problem(10, 10, 90));
+    EXPECT_EQ(-1, Solve_Commission_Problem(10, 10, 91));
+    EXPECT_EQ(-1, Solve_Commission_Problem(10, 10, 92));
 }
 
-TEST(Solve_NextDate_Problem, DecisionTableTest) {
+TEST(Solve_Commission_Problem, DecisionTableTest) {
   // Test in decision table testing form.
- 
+    EXPECT_EQ(660, Solve_Commission_Problem(40, 40, 40));
+    EXPECT_EQ(0, Solve_Commission_Problem(-1, 10, 10));
+    EXPECT_EQ(-1, Solve_Commission_Problem(0, 10, 10));
+    EXPECT_EQ(-1, Solve_Commission_Problem(71, 10, 10));
+    EXPECT_EQ(-1, Solve_Commission_Problem(10, 0, 10));
+    EXPECT_EQ(-1, Solve_Commission_Problem(10, 81, 10));
+    EXPECT_EQ(-1, Solve_Commission_Problem(10, 10, 0));
+    EXPECT_EQ(-1, Solve_Commission_Problem(10, 10, 91));
 }
 
 }
