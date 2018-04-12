36c5528528fb6ceb660156e2017c1f1f64b6edb4
Jay <jj011644@gmail.com>
2018-04-12 20:59:46 +0800

    add feature code

--- HW1/Commission_Problem/Commission_Problem.cc
+++ HW1/Commission_Problem/Commission_Problem.cc
@@ -5,6 +5,28 @@
 using namespace std;
 
 double Solve_Commission_Problem(int lock, int stock, int barrel) {
-	return 0;
+    double sales;
+    double commission;
+    if (lock == -1)
+        return 0;
+    else if (lock < 1 || lock > 70)
+        return -1;
+    if (stock < 1 || stock > 80)
+        return -1;
+    if (barrel < 1 || barrel > 90)
+        return -1;
+    sales = lock * 45 + stock * 30 + barrel * 25;
+    if (sales < 1000)
+        commission = sales * 0.1;
+    else if (sales < 1800) {
+        commission = 1000.0 * 0.1;
+        commission += (sales - 1000) * 0.15;
+    }
+    else {
+        commission = 1000 * 0.1;
+        commission += 800 * 0.15;
+        commission += (sales - 1800) * 0.2;
+    }
+    return commission;
 }
 
