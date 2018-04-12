a6b7d0a46039c885861c3013ba291355526a280b
Jay <jj011644@gmail.com>
2018-04-12 17:08:35 +0800

    initialize unittest.cc file

---
+++ HW1/Commission_Problem/Commission_Problem_Unittest.cc
@@ -0,0 +1,48 @@
+#include <limits.h>
+#include "Commission_Problem.h"
+#include "gtest/gtest.h"
+namespace {
+
+
+TEST(Solve_NextDate_Problem, NormalBoundaryTest) {
+	// Test in normal boundary value form.
+
+}
+
+TEST(Solve_NextDate_Problem, RobustBoundaryTest) {
+	// Test in robust boundary value form.
+
+}
+
+TEST(Solve_NextDate_Problem, WeakNormalEquivalenceTest) {
+  // Test in weak normal equivalence class form.
+
+}
+
+TEST(Solve_NextDate_Problem, StrongNormalEquivalenceTest) {
+  // Test in strong normal equivalence class form.
+
+}
+
+
+TEST(Solve_NextDate_Problem, WeakRobustEquivalenceTest) {
+  // Test in weak robust equivalence class form.
+
+}
+
+TEST(Solve_NextDate_Problem, StrongRobustEquivalenceTest) {
+  // Test in strong robust equivalence class form.
+
+}
+
+TEST(Solve_NextDate_Problem, EdgeTest) {
+  // Test in edge testing form.
+
+}
+
+TEST(Solve_NextDate_Problem, DecisionTableTest) {
+  // Test in decision table testing form.
+ 
+}
+
+}
