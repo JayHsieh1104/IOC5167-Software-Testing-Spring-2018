9c7582dfe099226d5919842188cdb63f4667b41e
Jay <jj011644@gmail.com>
2018-04-12 21:13:21 +0800

    fix bugs

--- HW1/Commission_Problem/Commission_Problem_Unittest.cc
+++ HW1/Commission_Problem/Commission_Problem_Unittest.cc
@@ -10,19 +10,19 @@ namespace {
 
 TEST(Solve_Commission_Problem, NormalBoundaryTest) {
 	// Test in normal boundary value form.
-	EXPECT_EQ(2800, Solve_Commission_Problem(35, 40, 1)); // Barrel min
-	EXPECT_EQ(2825, Solve_Commission_Problem(35, 40, 2)); // Barrel min+
-	EXPECT_EQ(5000, Solve_Commission_Problem(35, 40, 89)); // Barrel max-
-	EXPECT_EQ(5025, Solve_Commission_Problem(35, 40, 90)); // Barrel max
-	EXPECT_EQ(2730, Solve_Commission_Problem(35, 1, 45)); // Stock min
-	EXPECT_EQ(2760, Solve_Commission_Problem(35, 2, 45)); // Stock min+
-	EXPECT_EQ(5070, Solve_Commission_Problem(35, 79, 45)); // Stock max-
-	EXPECT_EQ(5100, Solve_Commission_Problem(35, 80, 45)); // Stock max
-	EXPECT_EQ(2370, Solve_Commission_Problem(1, 40, 45)); // Lock min
-	EXPECT_EQ(2415, Solve_Commission_Problem(2, 40, 45)); // Lock min+
-	EXPECT_EQ(5430, Solve_Commission_Problem(69, 40, 45)); // Lock max-
-	EXPECT_EQ(5475, Solve_Commission_Problem(70, 40, 45)); // Lock max
-	EXPECT_EQ(3900, Solve_Commission_Problem(35, 40, 45)); // all normal
+	EXPECT_EQ(420, Solve_Commission_Problem(35, 40, 1)); // Barrel min
+	EXPECT_EQ(425, Solve_Commission_Problem(35, 40, 2)); // Barrel min+
+	EXPECT_EQ(860, Solve_Commission_Problem(35, 40, 89)); // Barrel max-
+	EXPECT_EQ(865, Solve_Commission_Problem(35, 40, 90)); // Barrel max
+	EXPECT_EQ(406, Solve_Commission_Problem(35, 1, 45)); // Stock min
+	EXPECT_EQ(412, Solve_Commission_Problem(35, 2, 45)); // Stock min+
+	EXPECT_EQ(874, Solve_Commission_Problem(35, 79, 45)); // Stock max-
+	EXPECT_EQ(880, Solve_Commission_Problem(35, 80, 45)); // Stock max
+	EXPECT_EQ(334, Solve_Commission_Problem(1, 40, 45)); // Lock min
+	EXPECT_EQ(343, Solve_Commission_Problem(2, 40, 45)); // Lock min+
+	EXPECT_EQ(946, Solve_Commission_Problem(69, 40, 45)); // Lock max-
+	EXPECT_EQ(955, Solve_Commission_Problem(70, 40, 45)); // Lock max
+	EXPECT_EQ(640, Solve_Commission_Problem(35, 40, 45)); // all normal
 }
 
 TEST(Solve_Commission_Problem, RobustBoundaryTest) {
@@ -41,12 +41,12 @@ TEST(Solve_Commission_Problem, RobustBoundaryTest) {
 
 TEST(Solve_Commission_Problem, WeakNormalEquivalenceTest) {
   // Test in weak normal equivalence class form.
-	EXPECT_EQ(3900, Solve_Commission_Problem(35, 40, 45)); 
+	EXPECT_EQ(640, Solve_Commission_Problem(35, 40, 45)); 
 }
 
 TEST(Solve_Commission_Problem, StrongNormalEquivalenceTest) {
   // Test in strong normal equivalence class form.
-	EXPECT_EQ(3900, Solve_Commission_Problem(35, 40, 45)); 
+	EXPECT_EQ(640, Solve_Commission_Problem(35, 40, 45)); 
 }
 
 TEST(Solve_Commission_Problem, WeakRobustEquivalenceTest) {
