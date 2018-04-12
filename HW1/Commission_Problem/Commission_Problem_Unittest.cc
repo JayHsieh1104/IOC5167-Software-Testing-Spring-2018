b0db45e3635005e7ad97a939fc87220b76d9befb
Jay <jj011644@gmail.com>
2018-04-12 18:32:44 +0800

    add NormalBoundaryTest

--- HW1/Commission_Problem/Commission_Problem_Unittest.cc
+++ HW1/Commission_Problem/Commission_Problem_Unittest.cc
@@ -3,10 +3,25 @@
 #include "gtest/gtest.h"
 namespace {
 
+// 1 ≤ fans ≤ 70 
+// 1 ≤ pumps ≤ 80 
+// 1 ≤ bodies ≤ 90
 
 TEST(Solve_NextDate_Problem, NormalBoundaryTest) {
 	// Test in normal boundary value form.
-
+	EXPECT_EQ(2800, Solve_Commission_Problem(35, 40, 1)); // Barrel min
+	EXPECT_EQ(2825, Solve_Commission_Problem(35, 40, 2)); // Barrel min+
+	EXPECT_EQ(5000, Solve_Commission_Problem(35, 40, 89)); // Barrel max-
+	EXPECT_EQ(5025, Solve_Commission_Problem(35, 40, 90)); // Barrel max
+	EXPECT_EQ(2730, Solve_Commission_Problem(35, 1, 45)); // Stock min
+	EXPECT_EQ(2760, Solve_Commission_Problem(35, 2, 45)); // Stock min+
+	EXPECT_EQ(5070, Solve_Commission_Problem(35, 79, 45)); // Stock max-
+	EXPECT_EQ(5100, Solve_Commission_Problem(35, 80, 45)); // Stock max
+	EXPECT_EQ(2370, Solve_Commission_Problem(1, 40, 45)); // Lock min
+	EXPECT_EQ(2415, Solve_Commission_Problem(2, 40, 45)); // Lock min+
+	EXPECT_EQ(5430, Solve_Commission_Problem(69, 40, 45)); // Lock max-
+	EXPECT_EQ(5475, Solve_Commission_Problem(70, 40, 45)); // Lock max
+	EXPECT_EQ(3900, Solve_Commission_Problem(35, 40, 45)); // all normal
 }
 
 TEST(Solve_NextDate_Problem, RobustBoundaryTest) {
