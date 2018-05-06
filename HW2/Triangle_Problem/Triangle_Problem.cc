c83b5780528e9e732af949b2af41b37ae2b6f0c1
Jay <jj011644@gmail.com>
2018-05-06 23:10:47 +0800

    initialize triangle problem files

---
+++ HW2/Triangle_Problem/Triangle_Problem.cc
@@ -0,0 +1,18 @@
+#include "Triangle_Problem.h"
+#include <cstdio>
+
+using namespace std;
+
+char* Solve_Triangle_Problem(int a, int b, int c) {
+	if (a < 1 || b < 1 || c < 1 || a > 200 || b > 200 || c > 200)
+		return "Invalid Input";
+	if( !(a + b > c && a + c > b && b + c > a) )
+		return "Not a triangle";
+	if( a==b || b==c || c==a){
+		if (a==b && b==c && c==a)
+			return "Equilateral";
+		else
+			return "Isosceles";
+	}
+	return "Scalene";
+}
---
+++ HW2/Triangle_Problem/Triangle_Problem.h
@@ -0,0 +1,9 @@
+#ifndef GTEST_TRIANGLE_PROBLEM_H_
+#define GTEST TRIANGLE_PROBLEM_H_
+
+#include <string>
+
+// Print the outcome message based on the three input numbers.
+char* Solve_Triangle_Problem(int a, int b, int c);
+
+#endif //GTEST_TRIANGLE_PROBLEM_H_
