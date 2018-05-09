a1d4b3ea1e95de9ed938425ad66d46952a8beaea
Jay <jj011644@gmail.com>
2018-05-09 22:30:43 +0800

    add path testing with C1 coverage

--- HW2/Triangle_Problem/Triangle_Problem_Unittest.cc
+++ HW2/Triangle_Problem/Triangle_Problem_Unittest.cc
@@ -7,11 +7,20 @@ namespace {
 
 // C0 coverage
 TEST(Solve_Triangle_Problem, C0Test) {
-    EXPECT_STREQ("Not a triangle", Solve_Triangle_Problem(50,50,199));
+    EXPECT_STREQ("Not a triangle", Solve_Triangle_Problem(1,2,3));
     EXPECT_STREQ("Equilateral", Solve_Triangle_Problem(5,5,5));
     EXPECT_STREQ("Isosceles", Solve_Triangle_Problem(5,5,1));
-    EXPECT_STREQ("Scalene", Solve_Triangle_Problem(3,5,4));
-    EXPECT_STREQ("Out of range", Solve_Triangle_Problem(5,5,0));
+    EXPECT_STREQ("Scalene", Solve_Triangle_Problem(3,4,5));
 }
+
+// C1 coverage
+TEST(Solve_Triangle_Problem, C0Test) {
+    EXPECT_STREQ("Not a triangle", Solve_Triangle_Problem(1,2,3));
+    EXPECT_STREQ("Equilateral", Solve_Triangle_Problem(5,5,5));
+    EXPECT_STREQ("Isosceles", Solve_Triangle_Problem(5,5,1));
+    EXPECT_STREQ("Scalene", Solve_Triangle_Problem(3,4,5));
+    EXPECT_STREQ("Invalid Input", Solve_Triangle_Problem(5,5,0));
+}
+
 
 }
