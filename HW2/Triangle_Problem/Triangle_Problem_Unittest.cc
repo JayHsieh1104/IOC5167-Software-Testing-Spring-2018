9d4b271d43b061d73ca2621018fd910ee76c8e1f
Jay <jj011644@gmail.com>
2018-05-09 22:47:32 +0800

    add path testing with C2 coverage

--- HW2/Triangle_Problem/Triangle_Problem_Unittest.cc
+++ HW2/Triangle_Problem/Triangle_Problem_Unittest.cc
@@ -22,5 +22,13 @@ TEST(Solve_Triangle_Problem, C1Test) {
     EXPECT_STREQ("Invalid Input", Solve_Triangle_Problem(5,5,0));
 }
 
+// C2 coverage
+TEST(Solve_Triangle_Problem, C2Test) {
+    EXPECT_STREQ("Not a triangle", Solve_Triangle_Problem(1,2,3));
+    EXPECT_STREQ("Equilateral", Solve_Triangle_Problem(5,5,5));
+    EXPECT_STREQ("Isosceles", Solve_Triangle_Problem(5,5,1));
+    EXPECT_STREQ("Scalene", Solve_Triangle_Problem(3,4,5));
+    EXPECT_STREQ("Invalid Input", Solve_Triangle_Problem(5,5,0));
+}
 
 }
