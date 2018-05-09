6e4faaf5e2236ab22a530fed4530b5bdebbc6198
Jay <jj011644@gmail.com>
2018-05-09 22:16:35 +0800

    fix bug

--- HW2/Triangle_Problem/Triangle_Problem_Unittest.cc
+++ HW2/Triangle_Problem/Triangle_Problem_Unittest.cc
@@ -7,12 +7,11 @@ namespace {
 
 // C0 coverage
 TEST(Solve_Triangle_Problem, C0Test) {
-    EXPECT_STREQ("Not a triangle", triangle(50,50,199));
-    EXPECT_STREQ("Equilateral", triangle(5,5,5));
-    EXPECT_STREQ("Isosceles", triangle(5,5,1));
-    EXPECT_STREQ("Scalene", triangle(3,5,4));
-    EXPECT_STREQ("Out of range", triangle(5,5,0));
+    EXPECT_STREQ("Not a triangle", Solve_Triangle_Problem(50,50,199));
+    EXPECT_STREQ("Equilateral", Solve_Triangle_Problem(5,5,5));
+    EXPECT_STREQ("Isosceles", Solve_Triangle_Problem(5,5,1));
+    EXPECT_STREQ("Scalene", Solve_Triangle_Problem(3,5,4));
+    EXPECT_STREQ("Out of range", Solve_Triangle_Problem(5,5,0));
 }
-
 
 }
