a21b1bc08bb5b6fd849236540814114138028d91
Jay <jj011644@gmail.com>
2018-05-10 00:26:10 +0800

    add path testing with MCDC coverage

--- HW2/Triangle_Problem/Triangle_Problem_Unittest.cc
+++ HW2/Triangle_Problem/Triangle_Problem_Unittest.cc
@@ -24,11 +24,30 @@ TEST(Solve_Triangle_Problem, C1Test) {
 
 // C2 coverage
 TEST(Solve_Triangle_Problem, C2Test) {
+	// Because Triangle Problem doesn't have a loop, it is same as C1 Test
     EXPECT_STREQ("Not a triangle", Solve_Triangle_Problem(1,2,3));
     EXPECT_STREQ("Equilateral", Solve_Triangle_Problem(5,5,5));
     EXPECT_STREQ("Isosceles", Solve_Triangle_Problem(5,5,1));
     EXPECT_STREQ("Scalene", Solve_Triangle_Problem(3,4,5));
     EXPECT_STREQ("Invalid Input", Solve_Triangle_Problem(5,5,0));
+}
+
+// MCDC coverage
+TEST(Solve_Triangle_Problem, MCDCTest) {
+	EXPECT_STREQ("Invalid Input", Solve_Triangle_Problem(1,1,201));
+	EXPECT_STREQ("Invalid Input", Solve_Triangle_Problem(1,201,1));
+	EXPECT_STREQ("Invalid Input", Solve_Triangle_Problem(201,1,1));
+	EXPECT_STREQ("Invalid Input", Solve_Triangle_Problem(1,1,0));
+	EXPECT_STREQ("Invalid Input", Solve_Triangle_Problem(1,0,1));
+	EXPECT_STREQ("Invalid Input", Solve_Triangle_Problem(0,1,1));
+    EXPECT_STREQ("Equilateral", Solve_Triangle_Problem(5,5,5));
+	EXPECT_STREQ("Not a triangle", Solve_Triangle_Problem(1,2,3));
+	EXPECT_STREQ("Not a triangle", Solve_Triangle_Problem(1,3,2));
+	EXPECT_STREQ("Not a triangle", Solve_Triangle_Problem(3,2,1));
+    EXPECT_STREQ("Isosceles", Solve_Triangle_Problem(5,5,1));
+	EXPECT_STREQ("Isosceles", Solve_Triangle_Problem(5,1,5));
+	EXPECT_STREQ("Isosceles", Solve_Triangle_Problem(1,5,5));
+    EXPECT_STREQ("Scalene", Solve_Triangle_Problem(3,4,5));
 }
 
 }
