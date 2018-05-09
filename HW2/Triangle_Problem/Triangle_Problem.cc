64b6e8f9535d1b13b2be01a180c88ff237f60fe1
Jay <jj011644@gmail.com>
2018-05-10 00:32:23 +0800

    Finish feature code

--- HW2/Triangle_Problem/Triangle_Problem.cc
+++ HW2/Triangle_Problem/Triangle_Problem.cc
@@ -4,7 +4,6 @@
 using namespace std;
 
 char* Solve_Triangle_Problem(int a, int b, int c) {
-/*
 	if (a < 1 || b < 1 || c < 1 || a > 200 || b > 200 || c > 200)
 		return "Invalid Input";
 	if( !(a + b > c && a + c > b && b + c > a) )
@@ -16,6 +15,4 @@ char* Solve_Triangle_Problem(int a, int b, int c) {
 			return "Isosceles";
 	}
 	return "Scalene";
-*/
-	return "Feature code is not implemented";
 }
