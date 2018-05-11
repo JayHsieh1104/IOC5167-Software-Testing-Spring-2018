6a3814d66fd9494d4e64a7e31c25daa48e4ee984
Jay <jj011644@gmail.com>
2018-05-11 21:03:57 +0800

    fix bug

--- HW2/NextDate_Problem/NextDate_Problem_Unittest.cc
+++ HW2/NextDate_Problem/NextDate_Problem_Unittest.cc
@@ -19,13 +19,6 @@ TEST(Solve_NextDate_Problem, C1_Testing) {
     EXPECT_STREQ("Invalid Input", Solve_NextDate_Problem(1811, 1, 1));
 }
 
-TEST(Solve_NextDate_Problem, C1_Testing) {
-    EXPECT_STREQ("2000/11/30", Solve_NextDate_Problem(2000, 11, 29)); 
-	EXPECT_STREQ("2000/12/1", Solve_NextDate_Problem(2000, 11, 30)); 
-	EXPECT_STREQ("2001/1/1", Solve_NextDate_Problem(2000, 12, 31)); 
-    EXPECT_STREQ("Invalid Input", Solve_NextDate_Problem(1811, 1, 1));
-}
-
 TEST(Solve_NextDate_Problem, C2_Testing) {
     EXPECT_STREQ("2000/11/30", Solve_NextDate_Problem(2000, 11, 29)); 
 	EXPECT_STREQ("2000/12/1", Solve_NextDate_Problem(2000, 11, 30)); 
